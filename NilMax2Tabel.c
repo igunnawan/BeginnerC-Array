f8c8e857640bf1efcd1571733af15ef03663b5c5
igunnawan <110109403+igunnawan@users.noreply.github.com>
2023-03-16 01:32:31 +0700

    Update NilMax2Tabel.c

--- NilMax2Tabel.c
+++ NilMax2Tabel.c
@@ -1,12 +1,18 @@
+/* Nama File : NilMax2Tabel.c */
+/* Deskripsi : Menampilkan nilai maksimum ke-2 dari tabel T yang sudah terdefinisi */
+/* Pembuat   : Muhamad Gunawan (24060122120016)*/
+/* Tanggal Pembuatan   : 13 Maret 2023*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int NilMax2Tabel(){
 
-    int *Elmt;
-    int i,j,k;
-    int n,sum;
-    int max,max2;
+    int *Elmt; /* Pointer ke Integer (Array) */
+    int i,j; /* Counter */
+    int n; /* Jumlah Element pada Array */
+    int max; /* Nilai maximum suatu Element pada Array */
+    int max2; /* Nilai maximum ke 2 suatu Element pada Array */ 
 
     Elmt = (int*)malloc(n*sizeof(int));
 
