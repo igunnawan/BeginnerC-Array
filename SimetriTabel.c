e87cc83099f21a75ec037dee139056c27cfcb8f7
igunnawan <110109403+igunnawan@users.noreply.github.com>
2023-03-16 01:35:49 +0700

    Update SimetriTabel.c

--- SimetriTabel.c
+++ SimetriTabel.c
@@ -3,20 +3,23 @@
 
 int SimetriTabel()
 {
-    int i,j,k;
-    int sum;
-    int *JumlahElmt1,*JumlahElmt2;
-    int masukkan1,masukkan2;
+    int i; /* Counter */
+    int count; /* Menghitung banyaknya Element pada suatu array yang tidak sama */
+    int *JumlahElmt1; /* Pointer to Integer (Array 1) */
+    int *JumlahElmt2; /* Pointer to Integer (Array 2) */
+    int masukkan1; /* Jumlah Element pada suatu Array Pertama */
+    int masukkan2; /* Jumlah Element pada suatu Array Kedua */
 
-    sum = 0;
-    JumlahElmt1 = (int*)malloc(masukkan1*sizeof(int));
-    JumlahElmt2 = (int*)malloc(masukkan2*sizeof(int));
+    count = 0;
 
     printf("Masukkan Element Array 1 : ");
     scanf("%d",&masukkan1);
     printf("Masukkan Elemet Array 2 : ");
     scanf("%d",&masukkan2);
-
+    
+    JumlahElmt1 = (int*)malloc(masukkan1*sizeof(int));
+    JumlahElmt2 = (int*)malloc(masukkan2*sizeof(int));
+    
     printf("============================================================");
 
     if (masukkan1 <= 0 && masukkan2 <= 0){
@@ -39,12 +42,12 @@ int SimetriTabel()
 
             for(i=0;i<masukkan1;i++){
                 if (JumlahElmt1[i] != JumlahElmt2[i]){
-                    sum += 1;
+                    count += 1;
                 }
             }
 
             printf("=======================================================");
-            if(sum > 0){
+            if(count > 0){
                 printf("\nArray Tidak Simetris");
             }
             else {
