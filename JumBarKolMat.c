d5b213d0b6849f98526159519a18a00a8dc8794e
igunnawan <110109403+igunnawan@users.noreply.github.com>
2023-03-16 01:20:38 +0700

    Update JumBarKolMat.c

--- JumBarKolMat.c
+++ JumBarKolMat.c
@@ -1,20 +1,25 @@
+/* Nama File : JumBarKolMat.c */
+/* Deskripsi : Menampilkan jumlah setiap baris dan kolom pada tabel T (matrik) */
+/* Pembuat   : Muhamad Gunawan (24060122120016)*/
+/* Tanggal Pembuatan   : 15 Maret 2023*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int JumBarKolMat (){
 
-    int i,j;
-    int Baris,Kolom;
-    int element;
-    int sum;
-
+    int i,j; /* Counter */
+    int Baris,Kolom; /* Jumlah Element Baris dan Jumlah Element Kolom */
+    int sum; /* Menjummlah Element */
+    int** T; /* Pointer 2d Array */
+    
     sum = 0;
     printf("Masukkan nilai Baris : ");
     scanf("%d",&Baris);
     printf("Masukkan nilai Kolom : ");
     scanf("%d",&Kolom);
 
-    int** T = (int**)malloc(Baris*sizeof(int*));
+    T = (int**)malloc(Baris*sizeof(int*));
     for (i=0;i<Baris;i++){
         T[i] = (int*)malloc(Kolom*sizeof(int));
     }
@@ -48,54 +53,4 @@ int JumBarKolMat (){
     }
     return 0;
 }
-/*#include <stdlib.h>
-#include <stdio.h>
 
-int JumBarKolMat (){
-
-    int i,j;
-    int Baris,Kolom;
-    int *T;
-    int element;
-    int sum;
-
-    T = (int*)malloc(Baris*Kolom*sizeof(int));
-
-    sum = 0;
-    printf("Masukkan nilai Baris : ");
-    scanf("%d",&Baris);
-    printf("Masukkan nilai Kolom : ");
-    scanf("%d",&Kolom);
-
-
-    if(Baris <= 0 && Kolom <= 0){
-        printf("Masukkan harus positif");
-    }
-    else {
-        for(i=0;i<Baris;i++){
-            for(j=0;j<Kolom;j++){
-                scanf("%d",(T + i*Kolom + j));
-            }
-        }
-
-        for(i=0;i<Baris;i++){
-            sum = 0;
-            for(j=0;j<Kolom;j++){
-                sum = sum + T[i];
-            }
-            printf("%d",T[i]);
-            printf("Baris ke : %d",sum);
-        }
-
-        for(i=0;i<Kolom;i++){
-            sum = 0;
-            for(j=0;j<Baris;j++){
-                sum = sum + T[i];
-            }
-            printf("%d",T[i]);
-            printf("Kolom ke : %d",i,sum);
-        }
-        free(T);
-    }
-    return 0;
-}*/
